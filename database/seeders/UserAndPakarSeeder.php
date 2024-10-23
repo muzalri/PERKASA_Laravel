256319908c82c23e7fb71ef9f0ceaa0b40fcecf6
Abiyya1 <daffaabiyya145@gmail.com>
2024-10-24 00:29:01 +0700

    Nambah seeder

--- database/seeders/UserAndPakarSeeder.php
+++ database/seeders/UserAndPakarSeeder.php
@@ -25,6 +25,20 @@ class UserAndPakarSeeder extends Seeder
             // ... tambahkan pengguna lain jika diperlukan ...
         ]);
 
+        DB::table('users')->insert([
+            [
+                'name' => 'Pengguna Biasa Aja',
+                'email' => 'cibe@gmail.com',
+                'password' => Hash::make('12345678'),
+                'no_hp' => '085817288710',
+                'alamat' => 'Jl. Cisangkui No 6, Kota Bogor',
+                'role' => 'user',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // ... tambahkan pengguna lain jika diperlukan ...
+        ]);
+
         // Seeder untuk Pakar
         DB::table('users')->insert([
             [
