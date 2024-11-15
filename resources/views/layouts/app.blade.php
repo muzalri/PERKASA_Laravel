7bb7dfd65894ae6accacc8d148c2d443aac44e0b
muzalri <88919404+muzalri@users.noreply.github.com>
2024-11-15 18:58:22 +0700

    coba host azure

--- resources/views/layouts/app.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- ... existing navigation items ... -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('guide_books.index') }}">Panduan</a>
-</li>
-<!-- ... existing navigation items ... -->
