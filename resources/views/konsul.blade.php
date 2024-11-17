1e8031525d50e9e0264d66a24a2ed33d0fd7c60a
muzalri <88919404+muzalri@users.noreply.github.com>
2024-11-17 11:44:17 +0700

    ngilangin view

--- resources/views/konsul.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.master')
-
-@section('title', 'Konsultasi')
-
-@section('content')
-<div class="page-heading">
-    <h3>Konsultasi Pakar</h3>
-</div>
-@endsection
--- resources/views/konsultasi/chat.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="{{ route('pesan.store') }}" enctype="multipart/form-data">
-    @csrf
-    // ... existing form fields ...
-    <img src="{{ asset('storage/' . $message->image) }}" alt="Pesan Gambar" class="max-w-sm">
-</form> 
